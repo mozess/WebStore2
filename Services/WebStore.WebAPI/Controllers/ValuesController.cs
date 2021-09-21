12a6e6f5632bb856ceeb2f966b40365ca0a81577
mozess <mozess2011@gmail.com>
2021-09-21 11:43:43 +0300

    Текстовый WebAPI

---
+++ Services/WebStore.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class ValuesController : ControllerBase
+    {
+        private static List<string> __Values = Enumerable
+            .Range(1, 10)
+            .Select(i => $"Values-{i}")
+            .ToList();
+
+        [HttpGet]
+        public IActionResult Get() => Ok(__Values);
+
+        [HttpGet("count")]
+        public IActionResult Count() => Ok(__Values.Count);
+
+        [HttpGet("{index}")]
+        [HttpGet("index[[{index}]]")]
+        public ActionResult<string> GetByIndex(int index)
+        {
+            if (index < 0)
+                return BadRequest();
+            if (index >= __Values.Count)
+                return NotFound();
+
+            return __Values[index];
+        }
+
+        [HttpPost]
+        [HttpPost("add")]
+        public IActionResult Add(string str)
+        {
+            __Values.Add(str);
+            var index = __Values.Count - 1;
+            return CreatedAtAction(nameof(GetByIndex), new {index});
+        }
+
+        [HttpPut("{index}")]
+        [HttpPut("edit/{index}")]
+        public ActionResult Replace(int index, string NewStr)
+        {
+            if (index < 0)
+                return BadRequest();
+            if (index >= __Values.Count)
+                return NotFound();
+
+            __Values[index] = NewStr;
+            return Ok();
+        }
+
+        [HttpDelete("{index}")]
+        public ActionResult Delete(int index)
+        {
+            if (index < 0)
+                return BadRequest();
+            if (index >= __Values.Count)
+                return NotFound();
+
+            __Values.RemoveAt(index);
+            return Ok();
+        }
+    }
+}
