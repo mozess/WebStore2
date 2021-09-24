7a29557e8280edfa5731ab452e362c1da6bd98fc
mozess <mozess2011@gmail.com>
2021-09-24 04:08:47 +0300

    Регистрация киента каталога товаров

--- UI/WebStore/Startup.cs
+++ UI/WebStore/Startup.cs
@@ -20,6 +20,7 @@ using WebStore.Services.Services.InCookies;
 using WebStore.Services.Services.InMemory;
 using WebStore.Services.Services.InSQL;
 using WebStore.WebAPI.Clients.Employees;
+using WebStore.WebAPI.Clients.Products;
 using WebStore.WebAPI.Clients.Values;
 
 namespace WebStore
@@ -90,12 +91,13 @@ namespace WebStore
 
             services.AddScoped<IEmployeesData, SqlEmployeesData>();
             services.AddScoped<ICartService, InCookiesCartService>();
-            services.AddScoped<IProductData, SqlProductData>();
+            //services.AddScoped<IProductData, SqlProductData>();
             services.AddScoped<IOrderService, SqlOrderService>();
 
             services.AddHttpClient("WebStoreAPI", client => client.BaseAddress = new Uri(Configuration["WebAPI"]))
                 .AddTypedClient<IValuesService,ValuesClient>()
                 .AddTypedClient<IEmployeesData, EmployeesClient>()
+                .AddTypedClient<IProductData,ProductsClient>()
                 ;
 
 
