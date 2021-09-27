a1a3383e79b21a44968ba0ed447e5c70a7d704e7
mozess <mozess2011@gmail.com>
2021-09-27 18:54:12 +0300

    Заготовки контроллеров WebAPI для системы Identity

--- Services/WebStore.WebAPI/Controllers/Identity/RolesApiController.cs
+++ Services/WebStore.WebAPI/Controllers/Identity/RolesApiController.cs
@@ -4,13 +4,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
 using WebStore.Interfaces;
 
 namespace WebStore.WebAPI.Controllers.Identity
 {
-    [Route(WebAPIAddresses.Identity.Roles)]
     [ApiController]
+    [Route(WebAPIAddresses.Identity.Roles)]
+
     public class RolesApiController : ControllerBase
     {
+        private readonly RoleStore<Role> _RoleStore;
+        public RolesApiController(WebStoreDB db)
+        {
+            _RoleStore = new RoleStore<Role>(db);
+        }
+        [HttpGet("all")]
+        public async Task<IEnumerable<Role>> GetAllUsers() => await _RoleStore.Roles.ToArrayAsync();
     }
 }
--- Services/WebStore.WebAPI/Controllers/Identity/UsersApiController.cs
+++ Services/WebStore.WebAPI/Controllers/Identity/UsersApiController.cs
@@ -4,13 +4,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
 using WebStore.Interfaces;
 
 namespace WebStore.WebAPI.Controllers.Identity
 {
-    [Route(WebAPIAddresses.Identity.Users)]
     [ApiController]
+    [Route(WebAPIAddresses.Identity.Users)]
+
     public class UsersApiController : ControllerBase
     {
+        public readonly UserStore<User,Role,WebStoreDB> _UserStore;
+        public UsersApiController(WebStoreDB db)
+        {
+            _UserStore = new UserStore<User, Role, WebStoreDB>(db);
+            //_UserStore.AutoSaveChanges = false;//выключение отслеживания изменений для повышения быстродействия
+        }
+
+        [HttpGet("all")]
+        public async Task<IEnumerable<User>> GetAllUsers() => await _UserStore.Users.ToArrayAsync();
     }
 }
