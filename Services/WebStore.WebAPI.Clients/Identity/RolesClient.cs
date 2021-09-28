0b7fb9c1af5b72a7c9b2e628be965616dc6391a3
mozess <mozess2011@gmail.com>
2021-09-28 10:23:21 +0300

    Заготовки клиентов WebApi системы Identity

---
+++ Services/WebStore.WebAPI.Clients/Identity/RolesClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Interfaces;
+using WebStore.WebAPI.Clients.Base;
+
+namespace WebStore.WebAPI.Clients.Identity
+{
+    public class RolesClient:BaseClient
+    {
+        public RolesClient(HttpClient Client) : base(Client,WebAPIAddresses.Identity.Roles) { }
+    }
+}
---
+++ Services/WebStore.WebAPI.Clients/Identity/UsersClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Interfaces;
+using WebStore.WebAPI.Clients.Base;
+
+namespace WebStore.WebAPI.Clients.Identity
+{
+    class UsersClient:BaseClient
+    {
+        public UsersClient(HttpClient Client) : base(Client, WebAPIAddresses.Identity.Users) { }
+    }
+}
