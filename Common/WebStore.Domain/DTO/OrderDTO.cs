d8db022526fc556c341e5078ca34082f2c3a4b41
mozess <mozess2011@gmail.com>
2021-09-30 00:48:26 +0300

    Документация моделей, используемых в WebAPI

--- Common/WebStore.Domain/DTO/OrderDTO.cs
+++ Common/WebStore.Domain/DTO/OrderDTO.cs
@@ -7,29 +7,42 @@ using WebStore.Domain.ViewModels;
 
 namespace WebStore.Domain.DTO
 {
+    /// <summary>Информация о создаваемом заказе</summary>
     public class CreateOrderDTO
     {
+        /// <summary>Модель заказа</summary>
         public OrderViewModel Order { get; set; }
+        /// <summary>Пункт заказа</summary>
         public IEnumerable<OrderItemDTO> Items { get; set; }
     }
-
+    /// <summary>Заказ</summary>
     public class OrderDTO
     {
+        /// <summary>Идентификатор</summary>
         public int Id { get; set; }
+        /// <summary>Название</summary>
         public string Name { get; set; }
+        /// <summary>Телефонный номер для связи</summary>
         public string Phone { get; set; }
+        /// <summary>Адрес доставки</summary>
         public string Address { get; set; }
+        /// <summary>Дата формирования</summary>
         public DateTime Date { get; set; }
+        /// <summary>Пункт заказа</summary>
         public IEnumerable<OrderItemDTO> Items { get; set; }
     }
+    /// <summary>Пункт заказа</summary>
     public class OrderItemDTO
     {
+        /// <summary>Идентификатор</summary>
         public int Id { get; set; }
+        /// <summary>Идентификатор товара</summary>
         public int ProductId { get; set; }
+        /// <summary>Цена</summary>
         public decimal Price { get; set; }
+        /// <summary>Количество</summary>
         public int Quantity { get; set; }
     }
-
     public static class OrderDTOMapper
     {
         public static OrderItemDTO ToDTO(this OrderItem Item) => Item is null
