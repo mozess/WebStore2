1ee2360de4bc4bea8e339021df4468cc8b24df5c
mozess <mozess2011@gmail.com>
2021-09-30 23:48:13 +0300

    Заготовка библиотеки системы логирования

---
+++ Common/WebStore.Logger/Log4NetLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Logger
+{
+    public class Log4NetLoggerFactory
+    {
+    }
+
+    public class Log4NetLoggerProvider
+    {
+    }
+
+    public class Log4NetLogger
+    {
+    }
+
+}
